9a098eb9018d381573de284d33257fc5c9b863bd
Taui Silva <tauisilva@gmail.com>
2024-03-04 18:06:47 -0300

    ajustes no design do card

--- src/app/components/repos/repos.component.ts
+++ src/app/components/repos/repos.component.ts
@@ -3,19 +3,20 @@ import { Component, OnInit, ViewEncapsulation, inject } from '@angular/core';
 import { CarouselModule } from 'primeng/carousel';
 import { DataViewModule } from 'primeng/dataview';
 import { TagModule } from 'primeng/tag';
+import { environment } from '../../../env/env';
 import { repos } from '../../models/dados';
 import { GithubService } from '../../services/github.service';
 import { fIsoDate } from '../../utils/data';
-
 @Component({
   selector: 'app-repos',
   standalone: true,
   imports: [
-    TagModule,
+    CarouselModule,
     DataViewModule,
-    NgIf, NgClass,
-    CarouselModule
+    NgClass, NgIf,
+    TagModule,
   ],
+
   templateUrl: './repos.component.html',
   styleUrl: './repos.component.scss',
   providers: [GithubService],
@@ -24,6 +25,7 @@ import { fIsoDate } from '../../utils/data';
 
 export class ReposComponent implements OnInit {
   private gitService = inject(GithubService);
+  isPort = environment.REPO_NAME;
   skeleton = {
     card: false,
     card_img: false
@@ -42,34 +44,53 @@ export class ReposComponent implements OnInit {
       { breakpoint: '599px', numVisible: 1, numScroll: 1 }
 
     ]
-    // this.repos = repos.slice(0, 10);
-    // console.log(this.repos.length)
     this.load();
-    // this.repos.forEach((r) => {
-    //   const l = r.lang;
-    //   r.langs = this.getLan(l);
-    // })
+    // this.loadLocal();
+  }
+
+  loadLocal() {
+    const rep = repos.slice(0, 10);
+    this.repos.forEach((r) => {
+      const l = r.lang;
+      r.langs = this.getLan(l);
+    })
+    const index = rep.findIndex(i => i.name === this.isPort);
+    if (index !== -1) {
+      rep.unshift(rep.splice(index, 1)[0]);
+    }
+    this.repos = rep;
   }
 
   load() {
-    this.gitService.getAll().subscribe((repos) => {
+    this.gitService.getAll().subscribe(async (repos) => {
       if (repos) {
-        const res = repos.slice(0, 10);
+        const res = repos.filter((obj: { description: string | null; }) => obj.description !== null && obj.description !== '');
+        console.log(res.length);
+
         res.forEach(async (r: any) => {
           const img = await this.gitService.getImg(r?.html_url).toPromise();
           r.img = img;
+
           const languages = await this.gitService.getLangs(r?.full_name).toPromise();
           r.langs = this.getLan(languages);
         });
+        if (typeof this.isPort === 'string') {
+          const index = res.findIndex((i: { name: string; }) => i.name === this.isPort);
+
+          if (index !== -1) {
+            res.unshift(res.splice(index, 1)[0]);
+          }
+        }
+
         this.repos = res;
       }
-    })
+    });
   }
+
+
   formatData(data: Date) {
     return fIsoDate(new Date(data));
   }
-
-
 
   getLan(data: any): any {
     const total: any = Object.values(data).reduce((acc: any, value: any) => acc + value, 0);
@@ -83,8 +104,12 @@ export class ReposComponent implements OnInit {
     return obj;
   }
 
-
   getRandomColor(): string {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    let color = '';
+    do {
+      color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    } while (color === '#FFFFFF' || color === '#000000');
+    return color;
   }
+
 }
--- src/env/env.ts
+++ src/env/env.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: false,
   API_GITHUB: 'https://api.github.com',
   USER_GITHUB: 'tauisilva',
+  REPO_NAME: 'Portfolio-app',
   FILE_INFOS: 'infos.json'
 }
