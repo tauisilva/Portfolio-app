c435e9a01dea12a25b670197846abe83025a80ac
Taui Silva Lima <tauisilva@gmail.com>
2025-03-01 11:10:21 -0300

    [Refactor] Upgrade to angular 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,12 @@
-import { NgClass, NgFor } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { ThemeService } from '@shared/theme.service';
-import { ButtonModule } from 'primeng/button';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [ButtonModule, NgClass, NgFor],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  viewProviders: [ThemeService],
+  styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'portfolio-app';
-
-  buttons = [
-    { icon: 'bi-linkedin', action: () => this.openLink('https://www.linkedin.com/in/taui-silva/') },
-    { icon: 'bi-github', action: () => this.openLink('https://github.com/tauisilva') }
-  ];
-
-  constructor(private theme: ThemeService) { }
-
-  ngOnInit() {
-    this.theme.init();
-  }
-
-  toggleDarkMode() {
-    this.theme.toggleDarkMode();
-  }
-
-  isDarkMode() {
-    return this.theme.darkMode;
-  }
-
-  openLink(type: string) {
-    window.open(type, '_blank');
-  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,29 +1,9 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import Aura from '@primeng/themes/aura';
-import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideClientHydration(withEventReplay()),
-    provideHttpClient(withFetch()),
-    provideAnimations(),
-    providePrimeNG({
-      theme: {
-        preset: Aura,
-        options: {
-          darkModeSelector: '.my-app-dark',
-          primary: 'noir',
-        }
-      },
-      ripple: true
-    })
-  ]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
 };
